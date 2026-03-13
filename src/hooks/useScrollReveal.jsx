13810f1e68eb921c801c7a7e610c8fa26193dad1
chaim <chaim@chaims-Mac-mini.local>
2026-03-13 09:11:35 -0300

    fix: address safari/iphone layout issues and optimize reveal animations

--- src/hooks/useScrollReveal.jsx
+++ src/hooks/useScrollReveal.jsx
@@ -5,8 +5,8 @@ export function useScrollReveal(options = {}) {
 
   useEffect(() => {
     const defaultOptions = {
-      threshold: 0.15,
-      rootMargin: "0px 0px -50px 0px",
+      threshold: 0.1,
+      rootMargin: "0px 0px -10px 0px",
       ...options
     };
 
